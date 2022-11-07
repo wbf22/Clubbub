7ad19ad941802bd632d6643977d90056710b6120
wbf22 <brandon@fivefowlers.com>
2022-11-07 11:15:40 -0700

    Initial commit

---
+++ app/src/main/java/com/freedommuskrats/clubbub/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,45 @@
+package com.freedommuskrats.clubbub.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.freedommuskrats.clubbub.R;
+import com.freedommuskrats.clubbub.databinding.FragmentSearchBinding;
+
+//import com.freedommuskrats.clubbub.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentSearchBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        LinearLayout scrollview = container.findViewById(R.id.scrollViewList);
+//        scrollview.addView();
+
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
